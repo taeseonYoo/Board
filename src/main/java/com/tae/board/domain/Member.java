79497339872283f4a7529e78b4eddc899a76e6cd
taeseonYoo <jkl0642@hansung.ac.kr>
2024-12-29 22:16:54 +0900

    도메인 작성

--- src/main/java/com/tae/board/domain/Member.java
+++ src/main/java/com/tae/board/domain/Member.java
@@ -1,16 +1,33 @@
 package com.tae.board.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
-@Setter
-public class Member {
+public class Member extends BaseTimeEntity{
+
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    /**
+     * 양방향 연관관계를 위해 생성함.
+     * mappedBy가 있는 쪽은 외래 키를 관리하지 않는다.
+     * mappedBy를 통해 연관 관계의 주인을 명시한다.
+     * 외래 키가 어떤 엔티티에서 관리되는지 알려준다.
+     */
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
 }
