e4ce5972e3dd148d3dcafdd9337bdd76fe775e5c
taeseonYoo <jkl0642@hansung.ac.kr>
2025-01-17 17:19:51 +0900

    리팩토링: 코드 간소화

--- src/main/java/com/tae/board/security/SecurityConfig.java
+++ src/main/java/com/tae/board/security/SecurityConfig.java
@@ -1,25 +1,23 @@
 package com.tae.board.security;
 
-import com.tae.board.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final MemberDetailService memberDetailService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF disabled explicitly
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/board","/","/auth/login",
                                 "/auth/register", "/css/**", "/js/**").permitAll()
@@ -42,18 +40,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, MemberDetailService memberDetailService)
-            throws Exception{
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        authenticationManagerBuilder
-                .userDetailsService(memberDetailService) // UserDetailsService 설정
-                .passwordEncoder(bCryptPasswordEncoder); // 비밀번호 인코더 설정
-
-        return authenticationManagerBuilder.build();
-    }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
