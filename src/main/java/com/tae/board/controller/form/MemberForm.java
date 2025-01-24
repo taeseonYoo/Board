39dfe8bfed47e8c24e8286763456dd7fa04f6c63
taeseonYoo <jkl0642@hansung.ac.kr>
2025-01-24 20:19:54 +0900

    refactor:회원가입 필드 검증 시 누락된 유효성 검사 추가

--- src/main/java/com/tae/board/controller/form/MemberForm.java
+++ src/main/java/com/tae/board/controller/form/MemberForm.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class MemberForm {
@@ -16,7 +15,7 @@ public class MemberForm {
     private String name;
 
     @NotEmpty(message = "{member.password.empty}")
-    @Size(min=4,max = 12,message = "{member.password.size}")
+    @Size(min=8,max = 16,message = "{member.password.size}")
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "{member.password.format}")
     private String password;
 
@@ -25,6 +24,8 @@ public class MemberForm {
     private String email;
 
     @NotEmpty(message = "{member.nickname.empty}")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "{member.nickname.format}")
+    @Size(min = 2,max = 8,message = "{member.nickname.size}")
     private String nickname;
 
     public MemberForm(String name, String password, String email, String nickname) {
